a8c600806272c0d3a3b1958bcaeeb8d79955f420
therise3107 <lyadav3107@gmail.com>
2015-02-27 18:27:07 +0530

    added gravatar and removed paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-<<<<<<< HEAD
-=======
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/, :less_than => 5.megabytes
-
->>>>>>> 65b991a... added gravatar
   has_many :projects
   has_many :comments
 
