c9459f795634ce28a65b81f4c8a0727c8fba62bd
therise3107 <lyadav3107@gmail.com>
2015-02-28 18:54:37 +0530

    added user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'projects#index'
-  devise_for :users
-  resources :dashboards
+  devise_for :users 
+  resources :dashboards, only: :index
   resources :projects do
     resources :comments
   end
+  resources :users, only: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
